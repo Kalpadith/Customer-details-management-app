00c78b10cbd83a328a4639d85aa25fd13429154c
Ishan Kalpadith <ikalpadith@gmail.com>
2023-10-04 00:33:43 +0530

    Customer details management application

---
+++ CustomerDetailsManagementApplication/CustomerDetailsManagementApp/Services/LoginService.cs
@@ -0,0 +1,16 @@
+﻿using CustomerDetailsManagementApp.Services.ServiceInterfaces;
+
+namespace CustomerDetailsManagementApp.Services
+{
+    public class LoginService : ILoginService
+    {
+        private readonly IConfiguration _configuration;
+
+        public LoginService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+      
+    }
+}
